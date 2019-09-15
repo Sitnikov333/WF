459672ca4d3f36b95817849c279efee1c351f0c1
Kilgord <54819029+Kilgord@users.noreply.github.com>
2019-09-15 22:35:21 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        static string Rand()
+        {
+            Random rand = new Random();
+
+            string temp = Convert.ToString(rand.Next(100));
+            return temp;
+
+        }
+
+        static int Fib(int first, int second)
+        {
+           
+            
+            
+                int sum = first + second;
+
+
+
+                first = second;
+            second = sum;
+
+
+           
+            return sum;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            label1.Text =Rand().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            label1.Text = Fib(1,0).ToString();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
